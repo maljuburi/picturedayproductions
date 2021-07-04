84714f8841e9b5c096ea154d56ace4d224600969
Muhammad Al Juburi <maljuburi89@gmail.com>
2021-07-04 14:27:43 -0700

    added Bio and First-Film sections, mobile first design

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,13 @@
-<div class="bg-gray-50">
+<div class="bg-gray-50 sticky top-0 z-50 shadow-md">
 	<div class="grid grid-cols-12 py-4 max-w-6xl mx-auto">
-		<h1 class="text-2xl uppercase col-span-6">Picture Day Productions</h1>
-		<nav class="flex justify-end col-span-6">
-			<a class="px-4" href=".">Home</a>
-			<a href="./contact">Contact</a>
+		<h1
+			class="text-lg md:text-2xl uppercase pb-4 md:pb-0 text-center md:text-left col-span-12 md:col-span-6 px-4"
+		>
+			Picture Day Productions
+		</h1>
+		<nav class="flex md:justify-end justify-center col-span-12 md:col-span-6">
+			<a class="pr-4" href=".">Bio</a>
+			<a class="pr-4" href="#first-movie">My First Movie</a>
 		</nav>
 	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,92 @@
 <svelte:head>
 	<title>Picture Day Productions</title>
 </svelte:head>
+
 <div class="py-6">
-	<h1 class="text-3xl text-center py-6">Welcome this is the home page</h1>
-	<div class="max-w-6xl mx-auto">
+	<div class="max-w-6xl mx-auto px-3">
 		<div class="grid grid-cols-12">
-			<div class="col-span-6 px-4">
+			<div class="col-span-12 md:col-span-6 md:pr-5 pb-5">
 				<img
 					class="rounded-lg overflow-hidden shadow-md"
-					src="https://picsum.photos/400/300"
+					src="https://res.cloudinary.com/maljuburi/image/upload/v1625291259/PictureDayProductions/JB_2020.jpg"
 					alt="random"
 					width="100%"
 				/>
 			</div>
 
-			<div class="col-span-6">
-				<h3 class="text-2xl pb-3">Topic Title</h3>
+			<div class="col-span-12 md:col-span-6">
+				<h3 class="text-4xl text-center md:text-left pb-3">Jennifer Bartels</h3>
 				<hr class="mb-6" />
 				<p class="text-gray-700">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, maiores doloribus.
-					Reprehenderit numquam eum doloribus aut itaque excepturi, optio quod aliquid dolores
-					laborum sint atque dignissimos. Alias esse autem quia? Lorem ipsum dolor, sit amet
-					consectetur adipisicing elit. Totam laudantium ipsa officia reiciendis ratione molestias
-					voluptate consequuntur quas iste soluta, accusantium exercitationem rem, obcaecati
-					consectetur ducimus perspiciatis veritatis! Dicta, sapiente! Lorem ipsum dolor sit amet
-					consectetur adipisicing elit. Saepe, maiores doloribus. Reprehenderit numquam eum
-					doloribus aut itaque excepturi, optio quod aliquid dolores laborum sint atque dignissimos.
-					Alias esse autem quia? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-					laudantium ipsa officia reiciendis ratione molestias voluptate consequuntur quas iste
-					soluta, accusantium exercitationem rem, obcaecati consectetur ducimus perspiciatis
-					veritatis! Dicta, sapiente!
+					I was born and raised in Upstate New York, where I make my home today. My work history is
+					varied and has always inclined toward the creative. Years of training in ballet and modern
+					dance led to a stint as a professional dancer with the Maude Baum and Company Dance
+					Theatre in Albany, NY. While living in Brooklyn for twelve years, I studied improv comedy
+					at the Upright Citizens Brigade in Manhattan and earned a degree in jewelry design from
+					The Fashion Institute of Technology.
+				</p>
+			</div>
+			<div class="col-span-12">
+				<p class="text-gray-700 pb-3">
+					I worked as a jeweler, first for Alex Sepkus (I’d sometimes find myself on the subway,
+					bound for Bergdorf's with tens of thousands of dollars’ worth of jewels in my handbag),
+					and later at DVBNY, where I handmade the designs of Elsa Peretti for Tiffany and Co. I've
+					also worked in hospitality, from chain restaurants to quaint neighborhood stalwarts to
+					celebrity chef-owned behemoths.
+				</p>
+				<p class="text-gray-700 pb-3">
+					When I decided to return to school, my intent was to pursue a degree in film studies, as I
+					had written the first draft of a feature-length romcom, as well as the first draft of a
+					series pilot. While my school of choice, SUNY Albany, did not offer a film studies major,
+					it did include a degree in documentary studies in its curriculum.
 				</p>
 			</div>
 		</div>
 	</div>
+	<div id="first-movie" />
+	<div class="max-w-6xl mx-auto bg-gray-100 px-3 py-5">
+		<div class="grid grid-cols-12">
+			<div class="col-span-12 order-2 md:col-span-6 md:pl-5 pb-5">
+				<video width="100%" height="auto" controls class="rounded-lg overflow-hidden shadow-md">
+					<source
+						src="https://res.cloudinary.com/maljuburi/video/upload/v1625292026/PictureDayProductions/Dear_Lily_Love_Patrick_Trailer.mp4"
+						type="video/mp4"
+					/>
+
+					Your browser does not support the video tag.
+				</video>
+			</div>
+			<div class="col-span-12 md:col-span-6 order-1">
+				<h3 class="text-4xl text-center md:text-left pb-3">My first film</h3>
+				<hr class="my-3 mb-7 col-span-12" />
+				<p class="text-gray-700 pb-3">
+					Dear Lily, Love Patrick, is an intimate portrait of my mother's friendship with Patrick
+					Cowley, disco producer, electronic music pioneer, and Hi-NRG music innovator. I originally
+					envisioned a broader overview of Patrick's career up until his death from AIDS in 1982,
+					but another pandemic hit just as I was beginning to arrange travel and interviews, and the
+					country went into lockdown as COVID-19 struck.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="max-w-6xl mx-auto px-3 py-5">
+		<div class="grid grid-cols-12">
+			<div class="col-span-12">
+				<p class="text-gray-700 pb-3">
+					Consequently, I had to let go of my original and, admittedly, loftier goal and focus on
+					the primary resources I had at hand, a manila envelope of keepsakes my mother had held
+					onto, which included some cherished letters to her from Patrick. I recognized them for the
+					treasures that they are and made them the backbone of the film.
+				</p>
+				<p class="text-gray-700 pb-3">
+					I continue to focus on my romcom screenplay, and on a possible web series in which I
+					explore my inner demons - with the help of a one-and-a-half-inch tall ceramic cat
+					figurine. I currently work full time as a community habilitation and respite care provider
+					for an amazing, neurodiverse nine-year-old who is autistic and has epilepsy, who every day
+					expands my horizons in ways I never thought possible.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div name="contact" />
 </div>
