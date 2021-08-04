c3502b29e38c6c1834ff7e0a9e0abbc90b39756a
Muhammad Al Juburi <maljuburi89@gmail.com>
2021-08-03 22:38:29 -0700

    contact form functionality

--- src/routes/api/contact.js
+++ src/routes/api/contact.js
@@ -1,7 +1,24 @@
+import * as sgMail from '@sendgrid/mail';
 export const post = (request) => {
+	const { VITE_SENDGRID_API_KEY, VITE_SENDER_EMAIL } = import.meta.env;
 	const formData = JSON.parse(request.body);
 	console.log('formData', formData);
-	// TODO: handle contact data
+	sgMail.setApiKey(VITE_SENDGRID_API_KEY);
+	const msg = {
+		to: VITE_SENDER_EMAIL, // Change to your recipient
+		from: VITE_SENDER_EMAIL, // Change to your verified sender
+		subject: `From Website - ${formData.name}`,
+		text: formData.message,
+		html: `FROM: ${formData.name} - ${formData.email} <strong>${formData.message}</strong>`
+	};
+	sgMail
+		.send(msg)
+		.then(() => {
+			console.log('Email sent');
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 
 	return {
 		body: formData
