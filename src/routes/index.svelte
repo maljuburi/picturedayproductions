d53b7f880a2b62f3b659a87525aad5486e99397b
Muhammad Al Juburi <maljuburi89@gmail.com>
2021-08-15 13:41:32 -0700

    trying netlify forms v14

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -195,9 +195,9 @@
 					<textarea id="message" name="message" rows="6" bind:value={formData.message} required />
 				</div>
 
-				<!-- <div class="form-section">
-					<div data-netlify-recaptcha="true" />
-				</div> -->
+				<div class="form-section">
+					<input name={'bot-field'} />
+				</div>
 
 				<div class="flex justify-between align-middle">
 					<button type="submit">Send</button>
